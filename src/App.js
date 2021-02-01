f1b37c633512a76b484195551d7a44e9a06ff94f
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-02-01 08:34:52 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from 'react'
 import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
   Code,
   Grid,
   theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+} from '@chakra-ui/react'
+import {ColorModeSwitcher} from 'components/lib'
 
 function App() {
   return (
@@ -19,24 +17,12 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Text>Welcome to Designvote</Text>
           </VStack>
         </Grid>
       </Box>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
