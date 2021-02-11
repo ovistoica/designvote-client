0885b79b2e1b1fb734594940e1b2ed0183feeb49
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-02-11 08:57:13 +0200

    Testing action

--- src/screens/design.js
+++ src/screens/design.js
@@ -14,6 +14,8 @@ import {useDropzone} from 'react-dropzone'
 import {HangedImage} from 'assets/icons'
 import {useUploadImage} from 'utils/file-upload'
 
+let a
+
 function ImageDropInput() {
   const bg = useColorModeValue('surface', 'gray.700')
   const {uploadImage: onDrop, isLoading, progress} = useUploadImage()
