c7e2f9ab7cd4fd68d0b2c4176a603b58557aa0aa
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-03-04 07:45:32 +0200

    Refactor and add share design modal

---
+++ src/screens/design/design-stats.js
@@ -0,0 +1,177 @@
+import * as React from 'react'
+import {ViewIcon} from '@chakra-ui/icons'
+import {
+  useColorModeValue,
+  Flex,
+  Stack,
+  Text,
+  Divider,
+  Button as ChakraButton,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalFooter,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  useToast,
+  useClipboard,
+  Spinner,
+} from '@chakra-ui/react'
+import {OpinionIcon, VotesIcon} from 'assets/icons'
+import {FaShare} from 'react-icons/fa'
+import {useNavigate} from 'react-router-dom'
+import {useDesign, usePublishDesign} from 'utils/designs'
+import {Button} from 'components/lib'
+
+function ShareModal({isOpen, onClose, designId}) {
+  const {
+    data: {design},
+  } = useDesign(designId)
+  const {mutate: publish} = usePublishDesign(designId)
+
+  // When user wants to share the link
+  React.useEffect(() => {
+    if (isOpen && !design.public) {
+      publish()
+    }
+  }, [design.public, publish, isOpen])
+
+  const link = `https://designvote.io/vote/${design.shortUrl}`
+  const linkBackground = useColorModeValue('gray.100', 'gray.600')
+  const toast = useToast()
+  const {onCopy} = useClipboard(link)
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Share design</ModalHeader>
+        <ModalCloseButton _focus="none" />
+        <ModalBody>
+          {design.public ? (
+            <FormControl>
+              <FormLabel color="info">Share link</FormLabel>
+              <Flex
+                bg={linkBackground}
+                p="0.8em"
+                justify="space-between"
+                align="center"
+                borderRadius="10em"
+                width="100%"
+              >
+                <Text>{link}</Text>
+                <Button
+                  borderRadius="10em"
+                  _focus="none"
+                  _active="none"
+                  bg="primary.500"
+                  color="white"
+                  onClick={() => {
+                    onCopy()
+                    toast({
+                      title: 'Link copied to clipboard.',
+                      description: 'Design link copied to clipboard',
+                      status: 'success',
+                      duration: 2000,
+                      isClosable: true,
+                    })
+                  }}
+                >
+                  Copy
+                </Button>
+              </Flex>
+            </FormControl>
+          ) : (
+            <Flex justify="center">
+              <Text>Generating link </Text>
+              <Spinner ml="1em" />
+            </Flex>
+          )}
+        </ModalBody>
+
+        <ModalFooter>
+          <ChakraButton mr={3} onClick={onClose}>
+            Cancel
+          </ChakraButton>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export function DesignStats({totalVotes, totalOpinions, designId}) {
+  const statsBg = useColorModeValue('white', 'gray.700')
+  const iconColor = useColorModeValue('#1A202C', 'rgba(255, 255, 255, 0.92)')
+  const navigate = useNavigate()
+  const {isOpen, onClose, onOpen} = useDisclosure()
+  return (
+    <>
+      <Flex
+        flexDir="column"
+        boxShadow="md"
+        w="70%"
+        bg={statsBg}
+        borderRadius="0.5em"
+        align="center"
+        justify="space-evenly"
+        overflow="hidden"
+      >
+        <Flex w="100%" justify="center" p="1em">
+          <Stack p="1em" w="49%" align="center">
+            <VotesIcon fill={iconColor} />
+            <Text fontWeight="bold">{totalVotes}</Text>
+            <Text>Votes</Text>
+          </Stack>
+          <Divider orientation="vertical" />
+          <Stack p="1em" w="49%" align="center">
+            <OpinionIcon fill={iconColor} />
+            <Text fontWeight="bold">{totalOpinions}</Text>
+            <Text>Opinions</Text>
+          </Stack>
+        </Flex>
+        <Flex w="100%">
+          <ChakraButton
+            h="3em"
+            borderRadius="0px"
+            variant="ghost"
+            borderWidth="0.1em"
+            borderBottomWidth="0em"
+            borderLeftWidth="0em"
+            borderRightWidth="0.05em"
+            leftIcon={<FaShare />}
+            onClick={onOpen}
+            w="100%"
+            color="brand.500"
+            _hover={{bg: 'brand.200', color: 'white'}}
+            _focus="none"
+            _active="none"
+          >
+            Share
+          </ChakraButton>
+          <ChakraButton
+            borderTopWidth="0.1em"
+            borderLeftWidth="0.05em"
+            borderBottomWidth="0em"
+            borderRightWidth="0em"
+            borderRadius="0px"
+            variant="ghost"
+            h="3em"
+            leftIcon={<ViewIcon />}
+            onClick={() => navigate(`/vote/${designId}`)}
+            w="100%"
+            color="brand.500"
+            _hover={{bg: 'brand.200', color: 'white'}}
+            _focus="none"
+            _active="none"
+          >
+            Preview
+          </ChakraButton>
+        </Flex>
+      </Flex>
+      <ShareModal isOpen={isOpen} onClose={onClose} designId={designId} />
+    </>
+  )
+}
---
+++ src/screens/design/design-version.js
@@ -0,0 +1,156 @@
+import {
+  Button as ChakraButton,
+  Circle,
+  Flex,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spinner,
+  Stack,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+  Progress,
+} from '@chakra-ui/react'
+import {DeleteResourceAlert} from 'components/lib'
+import {useDeleteDesignVersion} from 'utils/design-version'
+import {useDesign} from 'utils/designs'
+import {DeleteIcon, EditIcon} from '@chakra-ui/icons'
+import {HiDotsHorizontal} from 'react-icons/hi'
+import {FiLink} from 'react-icons/fi'
+import {Check} from 'assets/icons'
+import {getVotePercent} from 'utils/votes'
+
+export function DesignVersionMenu({versionId, designId}) {
+  const {mutate: deleteDesignVersion, isLoading} = useDeleteDesignVersion(
+    designId,
+  )
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  return (
+    <>
+      <Circle
+        position="absolute"
+        right="-2"
+        top="-2"
+        bg="info"
+        size="1.5em"
+        boxShadow="md"
+        opacity={isLoading ? 1 : 0}
+        transition="0.25s all"
+        _groupHover={{
+          opacity: 1,
+        }}
+      >
+        <Menu>
+          <MenuButton
+            as={ChakraButton}
+            variant="ghost"
+            size="sm"
+            w="1.5em"
+            h="1.5em"
+            onClick={e => e.stopPropagation()}
+            opacity={isLoading ? 1 : 0}
+            _groupHover={{opacity: 1}}
+            _focus={{border: 'none'}}
+            _active={{border: 'none'}}
+            _hover="none"
+          >
+            {isLoading ? <Spinner /> : <HiDotsHorizontal fill="white" />}
+          </MenuButton>
+          <MenuList>
+            <MenuItem icon={<EditIcon />}>Edit</MenuItem>
+            <MenuItem icon={<FiLink />}>Get link</MenuItem>
+            <MenuItem
+              icon={<DeleteIcon />}
+              onClick={e => {
+                e.stopPropagation()
+                onOpen()
+              }}
+            >
+              Delete
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Circle>
+      <DeleteResourceAlert
+        isOpen={isOpen}
+        onClose={onClose}
+        onDeletePress={() => deleteDesignVersion(versionId)}
+      />
+    </>
+  )
+}
+
+function VersionHeader({totalVotes, votes, designId, versionId, name}) {
+  const headerBg = useColorModeValue('white', 'gray.700')
+  const votePercent = getVotePercent(totalVotes, votes.length)
+  return (
+    <Flex
+      h="5em"
+      w="100%"
+      bg={headerBg}
+      align="center"
+      p="1em"
+      borderTopRightRadius="0.5em"
+      borderTopLeftRadius="0.5em"
+    >
+      <Check />
+      <Flex direction="column" pl="1em" w="100%">
+        <Stack direction="row" align="center" justify="space-between">
+          <Flex direction="column">
+            <Text textTransform="uppercase" fontSize="0.95rem" mb="0">
+              {name}
+            </Text>
+            <Text color="info" fontSize="0.8rem">
+              {votes.length} votes
+            </Text>
+          </Flex>
+          <Text fontSize="xl" color="brand.500" fontWeight="bold">
+            {votePercent}%
+          </Text>
+        </Stack>
+        <Progress
+          value={votePercent}
+          borderRadius="20em"
+          h="0.3em"
+          background="brand.200"
+          colorScheme="brand"
+        />
+      </Flex>
+    </Flex>
+  )
+}
+
+export function DesignVersion({versionId, designId}) {
+  const {data} = useDesign(designId)
+  const {
+    versions,
+    pictures,
+    design: {totalVotes},
+  } = data
+  const version = versions[versionId]
+  const pictureId = version.pictures[0]
+  const {uri} = pictures[pictureId] ?? 'not found'
+  return (
+    <Flex
+      direction="column"
+      role="group"
+      transition="0.25s all"
+      borderRadius="0.5em"
+      boxShadow="base"
+      align="center"
+      position="relative"
+    >
+      <DesignVersionMenu designId={designId} versionId={versionId} />
+      <VersionHeader
+        versionId={versionId}
+        name={version.name}
+        votes={version.votes}
+        totalVotes={totalVotes}
+      />
+      <Image src={uri} objectFit="contain" maxH="28em" w="100%" />
+    </Flex>
+  )
+}
