4a4f5dd66fd6f88d9895a8419244ad7c6e394506
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-03-11 11:33:13 +0200

    Fix linting

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,7 @@ const {spawnSync} = require('./utils')
 const branch =
   process.env.BRANCH || spawnSync('git rev-parse --abbrev-ref HEAD')
 
+// eslint-disable-next-line no-console
 console.log(`Building for branch "${branch}"`)
 
 spawnSync('npx react-scripts build --profile', {stdio: 'inherit'})
