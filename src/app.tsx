566c62e59d09d893dd4d991532894c1d68963afa
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-04-01 09:21:24 +0300

    Dark mode for feedback part

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import {FullPageSpinner} from 'components/lib'
 import {useAuth} from 'context/auth-context'
 
-const AuthenticatedApp = React.lazy(
-  () => import(/* webpackPrefetch: true */ './authenticated-app'),
+const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
+const UnauthenticatedApp = React.lazy(
+  () => import(/* webpackPrefetch: true */ './unauthenticated-app'),
 )
-const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'))
 
 function App() {
   const {isAuthenticated} = useAuth()
