953e019d87f331b5a91882eb61c4c482b685d5c0
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-04-01 08:41:53 +0300

    Test utils for testing breakpoints

---
+++ src/components/__tests__/nav.tsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {NavBar} from '../nav'
+
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  })
+})
+
+test.todo('renders out correctly')
--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,7 +1,13 @@
-import {ChakraProvider} from '@chakra-ui/react'
+import {
+  ChakraProvider,
+  extendTheme,
+  theme as defaultTheme,
+} from '@chakra-ui/react'
 import {render, RenderOptions} from '@testing-library/react'
 import {DefaultOptions, QueryClient, QueryClientProvider} from 'react-query'
 import {BrowserRouter} from 'react-router-dom'
+import {mode} from '@chakra-ui/theme-tools'
+import * as colors from 'styles/colors'
 
 const defaultOptions: DefaultOptions<{status: number}> = {
   queries: {
@@ -26,10 +32,58 @@ const queryClient = new QueryClient({
   defaultOptions: defaultOptions as DefaultOptions,
 })
 
+const theme = extendTheme({
+  fonts: {
+    ...defaultTheme.fonts,
+    heading: `"IBM Plex Serif", ${defaultTheme.fonts.heading}`,
+    body: `"Lato", ${defaultTheme.fonts.body}`,
+  },
+  colors: {
+    ...defaultTheme.colors,
+    background3: colors.background3,
+    info: colors.info,
+    surface: colors.surface,
+    primary: {
+      100: '#f6ab79',
+      200: '#f59d63',
+      300: '#f38f4d',
+      400: '#f28136',
+      500: '#f07320',
+      600: '#D6661C',
+      700: '#B05417',
+      800: '#70360F',
+      900: '#70360F',
+    },
+    brand: {
+      100: '#69c5c8',
+      200: '#50bcbf',
+      300: '#37b2b5',
+      400: '#1ea9ac',
+      500: '#059FA3',
+      600: '#058f93',
+      700: '#047f82',
+      800: '#046f72',
+      900: '#035f62',
+    },
+    textSecondary: '#747474',
+    textInfoLight: '#ABBFC8',
+  },
+  styles: {
+    global: props => ({
+      body: {
+        fontFamily: 'body',
+        color: mode('gray.800', 'whiteAlpha.900')(props),
+        bg: mode('background3', 'gray.800')(props),
+        lineHeight: 'base',
+      },
+    }),
+  },
+})
+
 const AllTheProviders: React.ComponentType = ({children}) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <BrowserRouter>{children}</BrowserRouter>
       </ChakraProvider>
     </QueryClientProvider>
