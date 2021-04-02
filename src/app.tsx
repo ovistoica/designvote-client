b55a9d0a5f912c43e773d8695ee08887366d64ba
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-04-02 09:21:18 +0300

    Added log rocket for analytics

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import * as React from 'react'
 import {FullPageSpinner} from 'components/lib'
 import {useAuth} from 'context/auth-context'
+import LogRocket from 'logrocket'
+
+LogRocket.init('zbpdjy/designvote')
 
 const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
 const UnauthenticatedApp = React.lazy(
@@ -8,7 +11,17 @@ const UnauthenticatedApp = React.lazy(
 )
 
 function App() {
-  const {isAuthenticated} = useAuth()
+  const {isAuthenticated, user} = useAuth()
+
+  React.useEffect(() => {
+    if (isAuthenticated && user && process.env.NODE_ENV === 'production') {
+      //Log current session in production
+      LogRocket.identify(user.sub, {
+        name: user.name,
+        email: user.email,
+      })
+    }
+  }, [isAuthenticated, user])
 
   return (
     <React.Suspense fallback={<FullPageSpinner />}>
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,4 +1,51 @@
 export interface User {
+  /**
+   * First name of user
+   */
   given_name: string
+
+  /**
+   * Last name of user
+   */
+  family_name: string
+
+  /**
+   * url of image adress
+   */
   picture: string
+
+  /**
+   * Email adress
+   */
+  email: string
+
+  /**
+   * If user veriified email adress
+   */
+  email_verified: boolean
+
+  /**
+   * Locale prefference. Default is en
+   */
+  locale: 'en' | 'fr'
+
+  /**
+   * Concatenation of first and last name
+   */
+  name: string
+
+  /**
+   * Chosen by user. Default is email
+   */
+  nickname: string
+
+  /**
+   * unique id by which user is identified
+   */
+  sub: string
+
+  /**
+   * Last update date
+   */
+  updated_at: string
 }
