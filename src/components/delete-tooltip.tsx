15c2df23d504fe7a8af054dde0d7605fede74d08
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-04-23 13:10:17 +0300

    Fix design tooltip

---
+++ src/components/delete-tooltip.tsx
@@ -0,0 +1,30 @@
+import {Circle} from '@chakra-ui/layout'
+import {useColorModeValue as mode} from '@chakra-ui/react'
+import {DeleteBin} from 'assets/icons'
+
+interface DeleteTooltipProps {
+  onClick: () => void
+}
+
+export function DeleteTooltip({onClick}: DeleteTooltipProps) {
+  return (
+    <Circle
+      position="absolute"
+      right="-2"
+      top="-2"
+      bg={mode('gray.50', 'gray.700')}
+      size="2em"
+      shadow="base"
+      opacity={0}
+      transition="0.25s all"
+      cursor="pointer"
+      zIndex="sticky"
+      onClick={() => onClick()}
+      _groupHover={{
+        opacity: 1,
+      }}
+    >
+      <DeleteBin />
+    </Circle>
+  )
+}
