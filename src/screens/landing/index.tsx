e361e2d6f3e223cd0cd58e44a403d18664330707
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-06-09 07:53:40 +0300

    Add campagin tracking on landing page

--- src/screens/landing/index.tsx
+++ src/screens/landing/index.tsx
@@ -6,10 +6,26 @@ import {FeaturesSection} from './features-section'
 import {PhoneSection} from './phone-section'
 import {CTASection} from './cta-section'
 import {Footer} from 'components/footer'
+import {Helmet} from 'react-helmet'
 
 export const LandingPage = () => {
   return (
     <Box>
+      {
+        // Track campaigns w clickmagick
+        process.env.NODE_ENV === 'production' ? (
+          <Helmet>
+            <script type="text/javascript">
+              {`var clickmagick_cmc = {
+        uid: '145419',
+        hid: '1945973',
+        utm_source: 'organic',
+      }`}
+            </script>
+            <script src="//cdn.clkmc.com/cmc.js"></script>
+          </Helmet>
+        ) : null
+      }
       <HeroSection />
       <TestimonialSection />
       <FeedbackSection />
