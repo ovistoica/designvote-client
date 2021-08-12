d2f8d80ab95302c13cdca90ca54c97dc2e8ca029
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-08-12 07:07:02 +0300

    Added splitBee for analytics

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,12 @@ import * as React from 'react'
 import {FullPageSpinner} from 'components/lib'
 import {useAuth} from 'context/auth-context'
 import LogRocket from 'logrocket'
+import SplitBee from '@splitbee/web'
 
 if (process.env.NODE_ENV === 'production') {
   LogRocket.init('zbpdjy/designvote')
+
+  SplitBee.init()
 }
 
 const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
